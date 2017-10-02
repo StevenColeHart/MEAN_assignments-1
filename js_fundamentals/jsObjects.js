3ca6e6ab26d0e9579349dbefe99378bbe6c6ff4c
ngm84 <nathaniel.g.moore@gmail.com>
2017-10-02 18:22:27 -0400

    fixed objects

--- js_fundamentals/jsObjects.js
+++ js_fundamentals/jsObjects.js
@@ -25,23 +25,12 @@ let users = {
        {'first_name' : 'Gordon', 'last_name' : 'Poe'}
     ]
  };
-console.log("EMPLOYEES");
-for(let i = 0; i < users.employees.length;i++){
- let namelen = users.employees[i].last_name.length  + users.employees[i].first_name.length;
- console.log(i+1 + " - " + users.employees[i].last_name.toUpperCase() + ", " + users.employees[i].first_name.toUpperCase() + " - " + namelen);
-}
 
-console.log("MANAGERS");
-for(let i = 0; i< users.managers.length; i++){
- let namelen = users.managers[i].last_name.length  + users.managers[i].first_name.length;
- console.log(i+1 + " - " + users.managers[i].last_name.toUpperCase() + ", " + users.managers[i].first_name.toUpperCase() + " - " + namelen);
+//why does dot.notation not work here? Why is [indexing] necessary to get into the value associated with the key?
+for (let group in users){
+  console.log(group.toUpperCase());
+  for(let i = 0; i < users[group].length;i++){
+    let namelen = users[group][i].last_name.length  + users[group][i].first_name.length;
+    console.log(i+1 + " - " + users[group][i].last_name.toUpperCase() + ", " + users[group][i].first_name.toUpperCase() + " - " + namelen);
+  }
 }
-
-//Why can't I do something like this? :
-//for (let role in users){
-  //console.log(role.toUpperCase());
-  //for(let i = 0; i < users.group.length;i++){
-  //let namelen = users.group[i].last_name.length  + users.group[i].first_name.length;
-  //console.log(i+1 + " - " + users.group[i].last_name + ", " + users.group[i].first_name + " - " + "namelen");
-//}
-//}
